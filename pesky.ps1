ee809c9002b24e5cbaae00753c6801ce57be4893
Francesco Di Sales <f.disales@hotmail.com>
2022-08-27 19:26:07 +0200

    Create pesky.ps1

---
+++ pesky.ps1
@@ -0,0 +1,8 @@
+Stop-Service DiagTrack
+Stop-Service EventLog
+Stop-Service PlugPlay
+Stop-Service ShellHWDetection
+Stop-Service TokenBroker
+Stop-Service Wcmsvc
+Stop-Service wuauserv
+Stop-Service WSearch
